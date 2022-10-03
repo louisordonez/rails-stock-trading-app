12bb1be0c3c22bed792160df3421acbd791c1ebb
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-10-03 20:30:34 +0800

    Install react

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :books
+    end
+  end
+
   devise_for :admins
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
---
+++ stock-trading-app/src/App.js
@@ -0,0 +1,36 @@
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import Books from "./components/Books";
+import { useState, useEffect } from "react";
+
+const API_URL = "http://localhost:3000/api/v1/books";
+
+const getAPIData = () => {
+  return axios.get(API_URL).then((response) => response.data);
+};
+
+function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+
+    getAPIData().then((items) => {
+      if (mounted) {
+        setBooks(items);
+      }
+    });
+
+    return () => (mounted = false);
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Books books={books}></Books>
+    </div>
+  );
+}
+
+export default App;
