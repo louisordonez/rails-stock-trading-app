6c9a5e74a94a644bcf2932cbcf54c3095f7460a0
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-10-08 11:38:40 +0800

    Format ruby files with prettier

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -5,11 +5,11 @@ class Api::V1::AuthenticationController < ApplicationController
     @user = User.find_by_email(params[:email])
     if (@user&.authenticate(params[:password]))
       if @user.email_verified
-        payload = { user_id: @user.id }
+        payload = { user_id: @user.id, role: @user.role.first.id }
         access_token = JsonWebToken.encode(payload)
         render json: { user: @user, 'access-token': access_token }, status: :ok
-      else 
-        render json: { error: { message: "Account needs to be verified." } }, status: :unauthorized
+      else
+        render json: { error: { message: 'Account needs to be verified.' } }, status: :unauthorized
       end
     else
       render json: { error: 'Invalid login credentials' }, status: :unauthorized
@@ -23,17 +23,22 @@ class Api::V1::AuthenticationController < ApplicationController
       @user = User.find_by_email(decoded[:user_email])
       raise ActiveRecord::RecordNotFound if !@user
       if @user.email_verified
-        render json: { message: "Account has already been verified." }, status: :accepted
+        render json: { message: 'Account has already been verified.' }, status: :accepted
       else
         @user.update(email_verified: true)
-        render json: { user: @user, message: "Email confirmed. Account has been verified." }, status: :ok
+        render json: { user: @user, message: 'Email confirmed. Account has been verified.' }, status: :ok
       end
     rescue ActiveRecord::RecordNotFound
-      render json: { error: { message: "Record not found" } }, status: :not_found
+      render json: { error: { message: 'Record not found' } }, status: :not_found
     rescue JWT::ExpiredSignature
-      render json: { error: { message: "Confirmation invalid. Verification token has expired" } }, status: :unprocessable_entity
+      render json: {
+               error: {
+                 message: 'Confirmation invalid. Verification token has expired'
+               }
+             },
+             status: :unprocessable_entity
     rescue JWT::DecodeError
-      render json: { error: { message: "Token invalid." } }, status: :unprocessable_entity
+      render json: { error: { message: 'Token invalid.' } }, status: :unprocessable_entity
     end
   end
 
@@ -43,14 +48,18 @@ class Api::V1::AuthenticationController < ApplicationController
       @user = User.find_by_email(email)
       raise ActiveRecord::RecordNotFound if !@user
       if @user.email_verified
-        render json: { message: "Account has already been verified." }, status: :accepted
-      else 
+        render json: { message: 'Account has already been verified.' }, status: :accepted
+      else
         payload = { user_email: @user.email }
-        new_token = JsonWebToken.encode(payload, 24.hours.from_now)
-        render json: { email_token: new_token, message: "A confirmation email has been sent to verify your account." }, status: :ok
+        new_token = JsonWebToken.encode(payload, 30.minutes.from_now)
+        render json: {
+                 email_token: new_token,
+                 message: 'A confirmation email has been sent to verify your account.'
+               },
+               status: :ok
       end
     rescue ActiveRecord::RecordNotFound
-      render json: { error: { message: "Record not found" } }, status: :not_found
+      render json: { error: { message: 'Record not found' } }, status: :not_found
     end
   end
 end
