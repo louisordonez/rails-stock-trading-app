6272cb8fdf6604830b508b0820739ec16577772c
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-10-10 16:50:37 +0800

    Setup many-to-many relationship between User, Portfolio, UserTransaction

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   require 'securerandom'
   has_secure_password
   has_and_belongs_to_many :roles
+  has_many :portfolios
+  has_many :user_transactions, through: :portfolios
 
   validates :first_name, presence: true
   validates :last_name, presence: true
