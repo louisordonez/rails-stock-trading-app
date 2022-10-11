935b7179b030efee589c80502f3691c2541e2a71
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-10-12 00:29:56 +0800

    Setup IEX API tokens in rails credentials

--- config/initializers/iex_client.rb
+++ config/initializers/iex_client.rb
@@ -1,5 +1,19 @@
 IEX::Api.configure do |config|
-  config.publishable_token = ENV['SANDBOX_PUBLISHABLE_TOKEN']
-  config.secret_token = ENV['SANDBOX_SECRET_TOKEN']
-  config.endpoint = ENV['SANDBOX_ENDPOINT']
+  # ENV
+  # config.publishable_token = ENV['PRODUCTION_PUBLISHABLE_TOKEN']
+  # config.secret_token = ENV['PRODUCTION_SECRET_TOKEN']
+  # config.endpoint = ENV['PRODUCTION_ENDPOINT']
+
+  # config.publishable_token = ENV['SANDBOX_PUBLISHABLE_TOKEN']
+  # config.secret_token = ENV['SANDBOX_SECRET_TOKEN']
+  # config.endpoint = ENV['SANDBOX_ENDPOINT']
+
+  # Rails Credentials
+  # config.publishable_token = Rails.application.credentials.iex.production.publishable_token
+  # config.secret_token = Rails.application.credentials.iex.production.secret_token
+  # config.endpoint = Rails.application.credentials.iex.production.endpoint
+
+  config.publishable_token = Rails.application.credentials.iex.sandbox.publishable_token
+  config.secret_token = Rails.application.credentials.iex.sandbox.secret_token
+  config.endpoint = Rails.application.credentials.iex.sandbox.endpoint
 end
