203fe23d2346fb408e52759d8c0c118e660f0200
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-10-12 11:30:39 +0800

    Define stock_info in Stocks controller

--- app/controllers/api/v1/stocks_controller.rb
+++ app/controllers/api/v1/stocks_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::StocksController < ApplicationController
+  def stock_info
+    symbol = params[:symbol]
+    client = IEX::Api::Client.new
+    begin
+      company = client.company(symbol)
+      logo = client.logo(symbol)
+      quote = client.quote(symbol)
+    rescue IEX::Errors::SymbolNotFoundError
+      render json: { error: { message: 'Symbol not found' } }
+    rescue IEX::Errors::ClientError
+      render json: { error: { message: 'Something went wrong. Could not retrieve requested information.' } }
+    else
+      render json: { company: company, logo: logo, quote: quote }
+    end
+  end
 end
