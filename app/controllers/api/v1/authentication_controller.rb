9c963a14e0044d77cbd06730f06f9d8b88501aa6
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-10-12 12:57:40 +0800

    Fix begin-rescue format to appropriate syntax usage

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -19,12 +19,6 @@ class Api::V1::AuthenticationController < ApplicationController
       decoded = JsonWebToken.decode(token)
       @user = User.find_by_email(decoded[:user_email])
       raise ActiveRecord::RecordNotFound if !@user
-      if @user.email_verified
-        render json: { message: 'Account has already been verified.' }, status: :accepted
-      else
-        @user.update(email_verified: true)
-        render json: { user: @user, message: 'Email confirmed. Account has been verified.' }, status: :ok
-      end
     rescue ActiveRecord::RecordNotFound
       render json: { error: { message: 'Record not found' } }, status: :not_found
     rescue JWT::ExpiredSignature
@@ -36,44 +30,61 @@ class Api::V1::AuthenticationController < ApplicationController
              status: :unprocessable_entity
     rescue JWT::DecodeError
       render json: { error: { message: 'Token invalid.' } }, status: :unprocessable_entity
+    else
+      if @user.email_verified
+        render json: { message: 'Account has already been verified.' }, status: :accepted
+      else
+        @user.update(email_verified: true)
+        render json: { user: @user, message: 'Email confirmed. Account has been verified.' }, status: :ok
+      end
     end
   end
 
   def request_token
     header = request.headers['Authorization']
-    access_token = header.split(' ').last if header
-    begin
-      decoded = JsonWebToken.decode(access_token)
-      @user = User.find(decoded[:user_id])
-      if @user.email_verified
-        render json: { message: 'Account has already been verified.' }, status: :accepted
+    if header
+      access_token = header.split(' ').last
+      begin
+        decoded = JsonWebToken.decode(access_token)
+        @user = User.find(decoded[:user_id])
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: { message: 'Record not found' } }, status: :not_found
       else
-        payload = { user_email: @user.email }
-        new_token = JsonWebToken.encode(payload, 24.hours.from_now)
-        render json: {
-                 email_token: new_token,
-                 message: 'A confirmation email has been sent to verify your account.'
-               },
-               status: :ok
+        if @user.email_verified
+          render json: { message: 'Account has already been verified.' }, status: :accepted
+        else
+          payload = { user_email: @user.email }
+          new_token = JsonWebToken.encode(payload, 24.hours.from_now)
+          render json: {
+                   email_token: new_token,
+                   message: 'A confirmation email has been sent to verify your account.'
+                 },
+                 status: :ok
+        end
       end
-    rescue ActiveRecord::RecordNotFound
-      render json: { error: { message: 'Record not found' } }, status: :not_found
+    else
+      render json: { error: { message: 'Please sign in to continue.' } }
     end
   end
 
   def check_role
     header = request.headers['Authorization']
-    access_token = header.split(' ').last if header
-    begin
-      decoded = JsonWebToken.decode(access_token)
-      @user = User.find(decoded[:user_id])
-      render json: { role: @user.roles.first.name }
-    rescue ActiveRecord::RecordNotFound
-      render json: { error: { message: 'Record not found' } }, status: :not_found
-    rescue JWT::ExpiredSignature
-      render json: { error: { message: 'Session has expired. Log in to continue.' } }, status: :unauthorized
-    rescue JWT::DecodeError
-      render json: { error: { message: 'Token invalid.' } }, status: :unprocessable_entity
+    if header
+      access_token = header.split(' ').last
+      begin
+        decoded = JsonWebToken.decode(access_token)
+        @user = User.find(decoded[:user_id])
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: { message: 'Record not found' } }, status: :not_found
+      rescue JWT::ExpiredSignature
+        render json: { error: { message: 'Session has expired. Log in to continue.' } }, status: :unauthorized
+      rescue JWT::DecodeError
+        render json: { error: { message: 'Token invalid.' } }, status: :unprocessable_entity
+      else
+        render json: { role: @user.roles.first.name }
+      end
+    else
+      render json: { error: { message: 'Please sign in to continue.' } }
     end
   end
 end
