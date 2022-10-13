eaf376dad8c22df7aa9897f63a765c6e2b1de57a
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-10-14 02:44:04 +0800

    Create Wallet model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_13_182615) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_13_184308) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_13_182615) do
     t.string "password_digest"
     t.boolean "email_verified", default: false
     t.boolean "trade_verified", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "wallets", force: :cascade do |t|
+    t.decimal "balance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
