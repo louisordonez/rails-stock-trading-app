2d292cea9e95b6ac5c08e8a3fc9ee468c90c7779
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-10-14 02:31:25 +0800

    Fix incorrect usage of mono-transitive models associations

---
+++ app/models/stock_transaction.rb
@@ -0,0 +1,4 @@
+class StockTransaction < ApplicationRecord
+  belongs_to :user
+  belongs_to :portfolio
+end
---
+++ db/migrate/20221013182615_create_stock_transactions.rb
@@ -0,0 +1,14 @@
+class CreateStockTransactions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :stock_transactions do |t|
+      t.string :action_type
+      t.decimal :stock_quantity
+      t.decimal :stock_price
+      t.decimal :total_amount
+      t.references :user, null: false, foreign_key: true
+      t.references :portfolio, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
