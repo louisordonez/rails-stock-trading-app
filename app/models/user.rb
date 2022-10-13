68b0d57e952d12c8d31f8e5808e6574f2d6016c8
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-10-14 02:32:44 +0800

    Return inital User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   has_and_belongs_to_many :roles
   has_many :stock_transactions
   has_many :portfolios, through: :stock_transactions
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
 end
