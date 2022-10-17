f1e768bc597811778149067bfa69b9a7d48f327a
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-10-18 00:07:56 +0800

    Removed books controller test

--- test/controllers/portfolios_controller_test.rb
+++ test/controllers/portfolios_controller_test.rb
@@ -1,8 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class PortfoliosControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get portfolios_index_url
-    assert_response :success
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
