705cd1c02f728790e48091059fae6cdc62eb7bd3
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-10-17 22:02:45 +0800

    Refactor restrict_user and restrict_admin

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::UsersController < ApplicationController
   skip_before_action :authenticate_request, :email_verified?, only: [:create_user]
+  before_action :restrict_user, only: %i[index show_user create_admin update_user destroy_user]
   before_action :set_user, only: %i[show_user update_user destroy_user]
 
   def index
     @users = User.all
-    render json: @users, status: :ok if admin_request
-    render json: { error: { message: 'Request Forbidden.' } }, status: :forbidden if user_request
+    render json: @users, status: :ok
   end
 
   def show_current
@@ -13,8 +13,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def show_user
-    render json: @user, status: :ok if admin_request
-    render json: { error: { message: 'Request Forbidden.' } }, status: :forbidden if user_request
+    render json: @user, status: :ok
   end
 
   def create_user
@@ -27,7 +26,7 @@ class Api::V1::UsersController < ApplicationController
       render json: {
                user: @user,
                email_token: email_token,
-               message: 'A confirmation email has been sent to verify your account.',
+               message: 'A confirmation email has been sent to verify your account.'
              },
              status: :created
       UserMailer.with(user: @user, email_token: email_token).confirm_email.deliver_now
@@ -37,23 +36,20 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create_admin
-    if admin_request
-      @user = User.new(user_params)
-      if @user.save
-        @user.roles << admin_role
-        payload = { user_email: @user.email }
-        email_token = JsonWebToken.encode(payload, 24.hours.from_now)
-        render json: {
-                 user: @user,
-                 email_token: email_token,
-                 message: 'A confirmation email has been sent to verify your account.',
-               },
-               status: :created
-      else
-        render json: { error: { messages: @user.errors.full_messages } }, status: :unprocessable_entity
-      end
+    @user = User.new(user_params)
+    if @user.save
+      @user.roles << admin_role
+      payload = { user_email: @user.email }
+      email_token = JsonWebToken.encode(payload, 24.hours.from_now)
+      render json: {
+               user: @user,
+               email_token: email_token,
+               message: 'A confirmation email has been sent to verify your account.'
+             },
+             status: :created
+    else
+      render json: { error: { messages: @user.errors.full_messages } }, status: :unprocessable_entity
     end
-    render json: { error: { message: 'Request Forbidden.' } }, status: :forbidden if user_request
   end
 
   def update_current
@@ -69,18 +65,15 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def update_user
-    if admin_request
-      if @user.roles.first == admin_role
-        render json: { error: { message: 'Cannot update Admin account' } }, status: :forbidden
+    if @user.roles.first == admin_role
+      render json: { error: { message: 'Cannot update Admin account' } }, status: :forbidden
+    else
+      if @user.update(user_update_params)
+        render json: { user: @user, message: 'User account has been updated.' }, status: :ok
       else
-        if @user.update(user_update_params)
-          render json: { user: @user, message: 'User account has been updated.' }, status: :ok
-        else
-          render json: { error: { messages: @user.errors.full_messages } }, status: :unprocessable_entity
-        end
+        render json: { error: { messages: @user.errors.full_messages } }, status: :unprocessable_entity
       end
     end
-    render json: { error: { message: 'Request Forbidden.' } }, status: :forbidden if user_request
   end
 
   def destroy_current
@@ -89,15 +82,12 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def destroy_user
-    if admin_request
-      if @user.roles.first == admin_role
-        render json: { error: { message: 'Cannot delete Admin account' } }, status: :forbidden
-      else
-        @user.destroy
-        render json: { message: 'User account has been deleted.' }, status: :ok
-      end
+    if @user.roles.first == admin_role
+      render json: { error: { message: 'Cannot delete Admin account.' } }, status: :forbidden
+    else
+      @user.destroy
+      render json: { message: 'User account has been deleted.' }, status: :ok
     end
-    render json: { error: { message: 'Request Forbidden.' } }, status: :forbidden if user_request
   end
 
   private
