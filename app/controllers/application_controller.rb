f00a817a5c30a3bf1b8cb479236a59600b89c11a
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-10-19 21:54:48 +0800

    Fix app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,11 @@
   private
 
   def admin_role
-    Role.find_by(:name 'admin')
+    Role.find_by(name: 'admin')
   end
 
   def user_role
-    Role.find_by(:name 'user')
+    Role.find_by(name: 'user')
   end
 
   def restrict_admin
