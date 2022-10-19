5242191590f928c405be788ff7fe5c6c4555411e
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-10-19 19:59:04 +0800

    Update build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,7 @@
 # exit on error
 set -o errexit
 
+curl --create-dirs -o $HOME/.postgresql/root.crt -O https://cockroachlabs.cloud/clusters/0e9cde74-b548-41f1-bdaa-f5fd25a65c6a/cert
 yarn install
 bundle install
 # bundle exec rake assets:precompile
