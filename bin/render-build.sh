2adfc16524b8bc2e49b1526887a1937142deded5
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-10-19 19:53:04 +0800

    Update render build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 yarn install
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
+# bundle exec rake assets:precompile
+# bundle exec rake assets:clean
 bundle exec rake db:migrate
 bunlde exec rake db:seed
