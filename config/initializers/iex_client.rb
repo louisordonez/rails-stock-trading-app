7864e4404826573f79e9638fcb99cb6bedbf9eae
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-10-20 01:16:59 +0800

    Update iex_client.rb

--- config/initializers/iex_client.rb
+++ config/initializers/iex_client.rb
@@ -9,11 +9,11 @@ IEX::Api.configure do |config|
   # config.endpoint = ENV['SANDBOX_ENDPOINT']
 
   # Rails Credentials
-  config.publishable_token = Rails.application.credentials.iex.production.publishable_token
-  config.secret_token = Rails.application.credentials.iex.production.secret_token
-  config.endpoint = Rails.application.credentials.iex.production.endpoint
+  # config.publishable_token = Rails.application.credentials.iex.production.publishable_token
+  # config.secret_token = Rails.application.credentials.iex.production.secret_token
+  # config.endpoint = Rails.application.credentials.iex.production.endpoint
 
-  # config.publishable_token = Rails.application.credentials.iex.sandbox.publishable_token
-  # config.secret_token = Rails.application.credentials.iex.sandbox.secret_token
-  # config.endpoint = Rails.application.credentials.iex.sandbox.endpoint
+  config.publishable_token = Rails.application.credentials.iex.sandbox.publishable_token
+  config.secret_token = Rails.application.credentials.iex.sandbox.secret_token
+  config.endpoint = Rails.application.credentials.iex.sandbox.endpoint
 end
